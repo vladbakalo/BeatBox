04118d3ab5b19d3aaf1202670ea15702c6589d85
vladbakalo <vladbakalo@gmail.com>
2017-03-24 11:34:08 +0200

    First commit in idea 17

--- src/Animate.java
+++ src/Animate.java
@@ -7,18 +7,18 @@ import java.awt.*;
 public class Animate {
     int x = 1;
     int y = 1;
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         Animate gui = new Animate();
         gui.go();
     }
-    public void go()
-    {
+
+    public void go() {
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         MyDrawP drawP = new MyDrawP();
         frame.getContentPane().add(drawP);
-        frame.setSize(500,270);
+        frame.setSize(500, 270);
         frame.setVisible(true);
         for (int i = 0; i < 124; i++, x++, y++) {
 
@@ -31,12 +31,12 @@ public class Animate {
             }
         }
     }
-    class MyDrawP extends JPanel
-    {
+
+    class MyDrawP extends JPanel {
         @Override
         protected void paintComponent(Graphics g) {
             g.setColor(Color.WHITE);
-            g.fillRect(0,0,500,250);
+            g.fillRect(0, 0, 500, 250);
             g.setColor(Color.BLUE);
             g.fillRect(x, y, 500 - x * 2, 250 - y * 2);
 
